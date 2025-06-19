92565b81d0f179db8f3af8f6b16c44c12ea0570c
austincwang <wang.austinc@gmail.com>
2025-06-19 11:54:55 -0400

    start

---
+++ src/main/java/com/austincwang/getup/GetupNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.austincwang.getup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetupNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetupNetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/austincwang/getup/GetupNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.austincwang.getup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetupNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
